982a5b284c1e585e57345ca2cba2153c829bc556
Leroy G <liguan698@gmail.com>
2023-03-13 22:32:11 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let greeting;
+  if (distance <= 400) {
+    greeting = 'This one is on me!';
+    return greeting;
+  } else if (400 < distance && distance < 2000) {
+    greeting = "That will be twenty bucks.";
+    return greeting;
+  } else if (2000 < distance && distance <= 2500) {
+    greeting = "I will gladly take your thirty bucks.";
+    return greeting;
+  } else {
+    greeting = "No can do.";
+    return greeting;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city === 'NYC'?"Ok, sounds good.":"No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye."
+  }
 }
